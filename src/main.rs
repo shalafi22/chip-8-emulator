7e894ee2c60e1a65a27a4d1f7dc0bcd92963505d
Kemal Mutluergil <kemalmutluergil@gmail.com>
2024-01-25 01:01:51 +0300

    Chip8 struct defined

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+pub struct Chip8 {
+    //The main memory of the machine, 4KB
+    pub memory: [u8; 4096],
+
+    //16 General purpose 8-bit registers V1 - Vf,
+    //Vf isn't used by programs, it is used by some instructions as flag
+    pub Vx: [u8 ;16],
+
+    //16-bit special register
+    pub I: u16,
+
+    //TODO: These registers are decremented at a rate of 60Hz when non-zero
+    pub delay_timer: u8,
+    pub sound_timer: u8,
+
+    pub PC: u16,
+    pub SP: u8,
+
+    pub stack: [u16; 16],
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
