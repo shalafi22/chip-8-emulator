a3d1c4020b0838a2d37412c6f9660d7c7a54ebe9
Kemal Mutluergil <kemalmutluergil@gmail.com>
2024-01-25 13:17:24 +0300

    Draw to window 60FPS, A, B debug configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::WindowCanvas;
 use std::time::Duration;
 use sdl2::rect;
 
@@ -31,30 +32,14 @@ pub struct Chip8Display {
     pub pixels: [[u8; 8];32]
 }
 
-impl Chip8Display {
-    pub fn draw(&self) {
-        for row in self.pixels {
-            for byte in row {
-                let mut mask = 0b10000000;
-                for _i in 0..8 {
-                    if byte & mask == mask {
-                        print!("*");
-                    } else {
-                        print!(" ");
-                    }
-                    mask = mask  >> 1;
-                }
-            }
-            print!("\n");
-        }
-    }
-}
-
-fn open_window() {
+/// open sdl2 window and create a new chip8 display
+/// draw display to screen in a loop
+/// display might onl be updated when necessary instead of 60 FPS for better optimization
+fn open_window(disp: &Chip8Display) {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("Chip-8 emulator", 640, 320)
         .position_centered()
         .build()
         .unwrap();
@@ -64,11 +49,6 @@ fn open_window() {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let rect = rect::Rect::new(10, 10, 20, 20);
-    canvas.set_draw_color(Color::RGB(60, 65, 44));
-    canvas.draw_rect(rect).unwrap();
-    canvas.fill_rect(rect).unwrap();
-    canvas.present();
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -76,16 +56,53 @@ fn open_window() {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
+                Event::KeyDown { keycode: Some(Keycode::A), .. } => {
+                    canvas.set_draw_color(Color::RGB(0, 0, 0));
+                    canvas.clear();
+                    canvas.present();
+                },
+                Event::KeyDown { keycode: Some(Keycode::B), .. } => {
+                    draw_display_to_window(&mut canvas, &disp);
+                }
                 _ => {}
             }
         }
 
-        canvas.present();
+        
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
 
 
+/// draws the pixels of Chip8isplay to SDL2 canvas
+/// pixels are represented as 10x10 rectangles
+/// called by the running loop of canvas window
+fn draw_display_to_window(canvas: &mut WindowCanvas, disp: &Chip8Display) {
+    let mut x = 0;
+    let mut y = 0;
+    canvas.set_draw_color(Color::RGB(100, 225, 0));
+    for row in disp.pixels {
+        x = 0;
+        for byte in row {
+            let mut mask = 0b10000000;
+            for _i in 0..8 {
+                if byte & mask == mask {
+                    let rect = rect::Rect::new(x, y, 10, 10);
+                    canvas.draw_rect(rect).unwrap();
+                    canvas.fill_rect(rect).unwrap();
+                } 
+                mask = mask  >> 1;
+                x += 10;
+            }
+        }
+        y += 10;
+    }
+    canvas.present();
+}
+
 fn main() {
-    open_window();
+    let my_disp = Chip8Display {
+        pixels: [[0xF3; 8]; 32]
+    };
+    open_window(&my_disp);
 }
